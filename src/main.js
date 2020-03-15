84aeb6fe5d92a9091e1fbaa6dc966af7f8abfbe8
Klaus <k.striessnig@gmail.com>
2020-03-15 10:37:37 +0100

    Vue-Skeleton

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue';
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
