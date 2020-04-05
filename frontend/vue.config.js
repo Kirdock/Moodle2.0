206a043e6779a698c20fb77db8132c4354d2608e
Klaus <k.striessnig@gmail.com>
2020-04-05 19:43:58 +0200

    Update database credentials

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,15 @@
 module.exports = {
     // Change build paths to make them Maven compatible
     // see https://cli.vuejs.org/config/
+    devServer: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8098', // this configuration needs to correspond to the Spring Boot backends' application.properties server.port
+          ws: true,
+          changeOrigin: true
+        }
+      }
+    },
     outputDir: 'target/dist',
     assetsDir: 'static'
   }
