c17f4ba309775abb4417a7de4983b3a7fb91f653
yodiack <pipp.peter@gmx.at>
2020-04-27 16:11:28 +0200

    remove data.sql

--- backend/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO role(name) VALUES('ROLE_USER');
-INSERT INTO role(name) VALUES('ROLE_MODERATOR');
-INSERT INTO role(name) VALUES('ROLE_ADMIN');
-
-
-Insert into user(email,password,username) values('hallo@gmx.at','$2a$10$1dCKuQoQqbBNCK.Rb8XQSemwqdHdVAcCTb1kUQLg2key/4VX./TvS','hallo');
