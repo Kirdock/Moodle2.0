23834a345a9047f7803707a229e11ae0ea1d94ca
yodiack <pipp.peter@gmx.at>
2020-05-10 11:38:47 +0200

    add findCourseBySemester

--- backend/src/main/java/com/aau/moodle20/repository/CourseRepository.java
+++ backend/src/main/java/com/aau/moodle20/repository/CourseRepository.java
@@ -3,11 +3,15 @@ package com.aau.moodle20.repository;
 import com.aau.moodle20.domain.Course;
 import com.aau.moodle20.domain.ESemesterType;
 import com.aau.moodle20.domain.Semester;
+import com.aau.moodle20.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.persistence.Column;
+import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course,Long> {
 
-
+    Optional<Course> findCourseBySemester( Semester semester);
 }
 
 
