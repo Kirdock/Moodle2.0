4ca904a39b9bc0a81a320cf86f68575d2bdcea18
yodiack <pipp.peter@gmx.at>
2020-05-23 20:10:28 +0200

    Update UpdateExerciseSheetRequest.java
    make attribute description nullable

--- backend/src/main/java/com/aau/moodle20/payload/request/UpdateExerciseSheetRequest.java
+++ backend/src/main/java/com/aau/moodle20/payload/request/UpdateExerciseSheetRequest.java
@@ -19,7 +19,6 @@ public class UpdateExerciseSheetRequest {
     private LocalDateTime issueDate;
     private Integer minKreuzel;
     private Integer minPoints;
-    @NotNull
     private String description;
 
 
