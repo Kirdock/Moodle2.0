c897021bbacc6bdb3e0d5c013806f982ad9ffd17
yodiack <pipp.peter@gmx.at>
2020-07-23 14:07:58 +0200

    #50 add json include non null

--- backend/src/main/java/com/aau/moodle20/validation/Violation.java
+++ backend/src/main/java/com/aau/moodle20/validation/Violation.java
@@ -1,5 +1,8 @@
 package com.aau.moodle20.validation;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Violation {
 
     private String result;
