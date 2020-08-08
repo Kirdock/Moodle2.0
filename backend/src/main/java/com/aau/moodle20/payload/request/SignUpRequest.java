e9df88f679260f4a79f1418c9614c6b5b26d2959
yodiack <pipp.peter@gmx.at>
2020-08-08 11:56:19 +0200

    #52 , add matriculationNumber and email Validation

--- backend/src/main/java/com/aau/moodle20/payload/request/SignUpRequest.java
+++ backend/src/main/java/com/aau/moodle20/payload/request/SignUpRequest.java
@@ -2,6 +2,7 @@ package com.aau.moodle20.payload.request;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 public class SignUpRequest {
 
@@ -9,6 +10,7 @@ public class SignUpRequest {
     private String username;
 
     @NotBlank
+    @Pattern(regexp = "^[0-9]{8}$")
     private String matriculationNumber;
 
     @NotBlank
@@ -16,6 +18,8 @@ public class SignUpRequest {
     @NotBlank
     private String surname;
     private Boolean isAdmin;
+
+    @Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
     private String email;
 
     public String getUsername() {
