3f6e5b9e90599146d2e782cd753aa4c00c6da8e8
yodiack <pipp.peter@gmx.at>
2020-09-01 20:00:00 +0200

    #52 , add checks for semester apis

--- backend/src/main/java/com/aau/moodle20/payload/request/CreateSemesterRequest.java
+++ backend/src/main/java/com/aau/moodle20/payload/request/CreateSemesterRequest.java
@@ -2,10 +2,15 @@ package com.aau.moodle20.payload.request;
 
 import com.aau.moodle20.constants.ESemesterType;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class CreateSemesterRequest {
 
+    @NotNull
+    @Min(value = 0)
     private Integer year;
-
+    @NotNull
     private ESemesterType type;
 
     public Integer getYear() {
