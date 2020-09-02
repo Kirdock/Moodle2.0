71c02fb42b2a183f1d7ff8215add7322a44702e4
yodiack <pipp.peter@gmx.at>
2020-09-02 19:29:31 +0200

    organize imports

--- backend/src/main/java/com/aau/moodle20/config/Config.java
+++ backend/src/main/java/com/aau/moodle20/config/Config.java
@@ -1,10 +1,8 @@
 package com.aau.moodle20.config;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
