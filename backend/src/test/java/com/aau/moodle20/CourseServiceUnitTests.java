2f215b421820037d096aeeceec323bd17191a95d
yodiack <pipp.peter@gmx.at>
2020-09-06 16:26:32 +0200

    #78 add unit test for course service

--- backend/src/test/java/com/aau/moodle20/CourseServiceUnitTests.java
+++ backend/src/test/java/com/aau/moodle20/CourseServiceUnitTests.java
@@ -1,44 +1,41 @@
 package com.aau.moodle20;
 
+import com.aau.moodle20.constants.ApiErrorResponseCodes;
 import com.aau.moodle20.entity.Course;
 import com.aau.moodle20.entity.Semester;
 import com.aau.moodle20.entity.User;
+import com.aau.moodle20.exception.ServiceException;
 import com.aau.moodle20.payload.request.CreateCourseRequest;
+import com.aau.moodle20.payload.request.UpdateCourseRequest;
 import com.aau.moodle20.payload.response.CourseResponseObject;
 import com.aau.moodle20.repository.CourseRepository;
 import com.aau.moodle20.repository.SemesterRepository;
 import com.aau.moodle20.repository.UserRepository;
 import com.aau.moodle20.services.CourseService;
 import com.aau.moodle20.services.UserDetailsImpl;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JsonParseException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -48,29 +45,44 @@ import static org.mockito.Mockito.when;
 public class CourseServiceUnitTests {
 
     private static final Long COURSE_ID = 200L;
+    private static final String COURSE_NUMBER= "123.123";
     private static final Long SEMESTER_ID = 12L;
     private static final String OWNER_MATRICULATION_NUMBER = "12345678";
 
     @InjectMocks
     private CourseService courseService;
-
     @Mock
     private CourseRepository courseRepository;
     @Mock
     private UserRepository userRepository;
     @Mock
     private SemesterRepository semesterRepository;
-
+    private String adminMatriculationNumber ="00000000";
+    private String normalMatriculationNumber = "12345678";
 
 
     @Before
     public void initMocks(){
         MockitoAnnotations.initMocks(this);
-        mockSecurityContext();
     }
 
     @Test
-    public void getCourse()  {
+    public void getCourse_Admin()  {
+        mockSecurityContext_WithUserDetails(getUserDetails_Admin());
+        Course course = getTestCourse();
+        when(courseRepository.findById(COURSE_ID)).thenReturn(Optional.of(course));
+
+        CourseResponseObject responseObject = course.createCourseResponseObject_GetCourse();
+        responseObject.setPresented(new ArrayList<>());
+        responseObject.setOwner(course.getOwner().getMatriculationNumber());
+
+        CourseResponseObject courseResponseObject = courseService.getCourse(200L);
+        assertEquals(responseObject,courseResponseObject);
+    }
+
+    @Test
+    public void getCourse_Not_Admin()  {
+        mockSecurityContext_WithUserDetails(getUserDetails_Not_Admin());
         Course course = getTestCourse();
         when(courseRepository.findById(COURSE_ID)).thenReturn(Optional.of(course));
 
@@ -83,34 +95,180 @@ public class CourseServiceUnitTests {
 
     @Test
     public void createCourse()  {
+        mockSecurityContext_WithUserDetails(getUserDetails_Admin());
         Course course = getTestCourse();
         when(userRepository.findByMatriculationNumber(OWNER_MATRICULATION_NUMBER)).thenReturn(Optional.of(new User(OWNER_MATRICULATION_NUMBER)));
         when(semesterRepository.findById(SEMESTER_ID)).thenReturn(Optional.of(new Semester(SEMESTER_ID)));
         when(courseRepository.save(any(Course.class))).thenReturn(course);
 
-
-        CreateCourseRequest createCourseRequest = new CreateCourseRequest();
-        createCourseRequest.setOwner(OWNER_MATRICULATION_NUMBER);
-        createCourseRequest.setDescription(course.getDescription());
-        createCourseRequest.setMinKreuzel(course.getMinKreuzel());
-        createCourseRequest.setMinPoints(course.getMinPoints());
-        createCourseRequest.setName(course.getName());
-        createCourseRequest.setNumber(course.getNumber());
-        createCourseRequest.setSemesterId(SEMESTER_ID);
+        CreateCourseRequest createCourseRequest = getCourseCreateRequest(course);
 
         CourseResponseObject courseResponseObject = courseService.createCourse(createCourseRequest);
         assertEquals(new CourseResponseObject(course.getId()),courseResponseObject);
     }
 
-    private void mockSecurityContext()
+    @Test
+    public void createCourse_where_Semester_does_not_exists()  {
+        mockSecurityContext_WithUserDetails(getUserDetails_Admin());
+        Course course = getTestCourse();
+        when(userRepository.findByMatriculationNumber(OWNER_MATRICULATION_NUMBER)).thenReturn(Optional.of(new User(OWNER_MATRICULATION_NUMBER)));
+        when(courseRepository.save(any(Course.class))).thenReturn(course);
+
+        CreateCourseRequest createCourseRequest = getCourseCreateRequest(course);
+
+        ServiceException exception = assertThrows(ServiceException.class, () -> {
+            courseService.createCourse(createCourseRequest);
+        });
+        String expectedMessage = "Error: Semester not found!";
+        assertEquals(expectedMessage,exception.getMessage());
+        assertEquals(exception.getHttpStatus(), HttpStatus.NOT_FOUND
+        );
+    }
+
+    @Test
+    public void createCourse_where_Owner_does_not_exists()  {
+        mockSecurityContext_WithUserDetails(getUserDetails_Admin());
+        Course course = getTestCourse();
+
+        when(semesterRepository.findById(SEMESTER_ID)).thenReturn(Optional.of(new Semester(SEMESTER_ID)));
+        when(courseRepository.save(any(Course.class))).thenReturn(course);
+
+        CreateCourseRequest createCourseRequest = getCourseCreateRequest(course);
+
+        ServiceException exception = assertThrows(ServiceException.class, () -> {
+            courseService.createCourse(createCourseRequest);
+        });
+        String expectedMessage = "Error: User not found!";
+        assertEquals(expectedMessage,exception.getMessage());
+        assertEquals(exception.getHttpStatus(), HttpStatus.NOT_FOUND
+        );
+    }
+
+    @Test
+    public void createCourse_where_Course_already_exists() {
+        mockSecurityContext_WithUserDetails(getUserDetails_Admin());
+        Course course = getTestCourse();
+        CreateCourseRequest createCourseRequest = getCourseCreateRequest(course);
+
+        when(userRepository.findByMatriculationNumber(OWNER_MATRICULATION_NUMBER)).thenReturn(Optional.of(new User(OWNER_MATRICULATION_NUMBER)));
+        when(semesterRepository.findById(SEMESTER_ID)).thenReturn(Optional.of(new Semester(SEMESTER_ID)));
+        when(courseRepository.save(any(Course.class))).thenReturn(course);
+        when(courseRepository.existsByNumberAndSemester_Id(COURSE_NUMBER, SEMESTER_ID)).thenReturn(Boolean.TRUE);
+
+
+        ServiceException exception = assertThrows(ServiceException.class, () -> {
+            courseService.createCourse(createCourseRequest);
+        });
+        String expectedMessage = "Course in Semester already exists";
+        assertEquals(expectedMessage,exception.getMessage());
+        assertEquals(exception.getErrorResponseCode(), ApiErrorResponseCodes.COURSE_IN_SEMESTER_ALREADY_EXISTS);
+    }
+
+    @Test
+    public void updateCourse_where_Course_does_not_exists() {
+        mockSecurityContext_WithUserDetails(getUserDetails_Admin());
+        Course course = getTestCourse();
+        UpdateCourseRequest updateCourseRequest = getUpdateCourseRequest(course);
+
+        ServiceException exception = assertThrows(ServiceException.class, () -> {
+            courseService.updateCourse(updateCourseRequest);
+        });
+        String expectedMessage = "Error: Course not found!";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage,exception.getMessage());
+        assertEquals(exception.getHttpStatus(), HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void updateCourse_where_new_course_number_already_exists() {
+        mockSecurityContext_WithUserDetails(getUserDetails_Admin());
+        Course course = getTestCourse();
+        when(courseRepository.existsByNumberAndSemester_Id("333.333", course.getSemester().getId())).thenReturn(Boolean.TRUE);
+        when(courseRepository.findById(course.getId())).thenReturn(Optional.of(course));
+        when(userRepository.existsByMatriculationNumber(course.getOwner().getMatriculationNumber())).thenReturn(Boolean.TRUE);
+        UpdateCourseRequest updateCourseRequest = getUpdateCourseRequest(course);
+        updateCourseRequest.setNumber("333.333");
+
+        ServiceException exception = assertThrows(ServiceException.class, () -> {
+            courseService.updateCourse(updateCourseRequest);
+        });
+        String expectedMessage = "Error: A Course with this number already exists";
+        assertEquals(expectedMessage,exception.getMessage());
+        assertEquals(exception.getErrorResponseCode(), ApiErrorResponseCodes.CHANGED_COURSE_NUMBER_ALREADY_EXISTS);
+    }
+
+    @Test
+    public void updateCourse_where_updated_Owner_does_not_exists() {
+        mockSecurityContext_WithUserDetails(getUserDetails_Admin());
+        Course course = getTestCourse();
+        UpdateCourseRequest updateCourseRequest = getUpdateCourseRequest(course);
+        when(courseRepository.findById(course.getId())).thenReturn(Optional.of(course));
+
+        ServiceException exception = assertThrows(ServiceException.class, () -> {
+            courseService.updateCourse(updateCourseRequest);
+        });
+        String expectedMessage = "Error: Owner cannot be updated because the given matriculationNumber those not exists!";
+        assertEquals(expectedMessage,exception.getMessage());
+        assertEquals(exception.getHttpStatus(), HttpStatus.NOT_FOUND);
+    }
+
+
+    private CreateCourseRequest getCourseCreateRequest(Course course)
     {
-        UserDetailsImpl applicationUser = mock(UserDetailsImpl.class);
-        applicationUser.setAdmin(Boolean.FALSE);
+        CreateCourseRequest createCourseRequest = new CreateCourseRequest();
+        createCourseRequest.setOwner(course.getOwner().getMatriculationNumber());
+        createCourseRequest.setDescription(course.getDescription());
+        createCourseRequest.setMinKreuzel(course.getMinKreuzel());
+        createCourseRequest.setMinPoints(course.getMinPoints());
+        createCourseRequest.setName(course.getName());
+        createCourseRequest.setNumber(course.getNumber());
+        createCourseRequest.setSemesterId(course.getSemester().getId());
+
+        return createCourseRequest;
+    }
+
+    private UpdateCourseRequest getUpdateCourseRequest(Course course)
+    {
+        UpdateCourseRequest updateCourseRequest = new UpdateCourseRequest();
+        updateCourseRequest.setOwner(course.getOwner().getMatriculationNumber());
+        updateCourseRequest.setDescription(course.getDescription());
+        updateCourseRequest.setMinKreuzel(course.getMinKreuzel());
+        updateCourseRequest.setMinPoints(course.getMinPoints());
+        updateCourseRequest.setName(course.getName());
+        updateCourseRequest.setNumber(course.getNumber());
+        updateCourseRequest.setId(course.getId());
+
+        return updateCourseRequest;
+    }
+
+
+
+    private void mockSecurityContext_WithUserDetails(UserDetailsImpl userDetails)
+    {
         Authentication authentication = mock(Authentication.class);
         SecurityContext securityContext = mock(SecurityContext.class);
         when(securityContext.getAuthentication()).thenReturn(authentication);
         SecurityContextHolder.setContext(securityContext);
-        when(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).thenReturn(applicationUser);
+        when(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).thenReturn(userDetails);
+    }
+
+    private UserDetailsImpl getUserDetails_Admin()
+    {
+        UserDetailsImpl userDetails = new UserDetailsImpl("admin","admin", Boolean.TRUE, adminMatriculationNumber,"admin","admin" );
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("Admin"));
+        userDetails.setAuthorities(authorities);
+        return userDetails;
+    }
+
+    private UserDetailsImpl getUserDetails_Not_Admin()
+    {
+        UserDetailsImpl userDetails = new UserDetailsImpl("normal","normal", Boolean.FALSE, normalMatriculationNumber,"normal","normal" );
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        userDetails.setAuthorities(authorities);
+        return userDetails;
     }
 
     private Course getTestCourse()
@@ -123,6 +281,7 @@ public class CourseServiceUnitTests {
         course.setMinKreuzel(20);
         course.setMinPoints(20);
         course.setName("dd");
+        course.setNumber(COURSE_NUMBER);
         course.setOwner(new User(OWNER_MATRICULATION_NUMBER));
         course.setSemester(new Semester(SEMESTER_ID));
         course.setExerciseSheets(new HashSet<>());
