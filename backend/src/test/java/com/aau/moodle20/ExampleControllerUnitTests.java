99fa05e0e64dc52a019421c751a7e6b324f79705
yodiack <pipp.peter@gmx.at>
2020-09-09 18:14:12 +0200

    #78 add example controller unit tests

--- backend/src/test/java/com/aau/moodle20/ExampleControllerUnitTests.java
+++ backend/src/test/java/com/aau/moodle20/ExampleControllerUnitTests.java
@@ -11,10 +11,12 @@ import com.aau.moodle20.repository.UserRepository;
 import com.aau.moodle20.services.CourseService;
 import com.aau.moodle20.services.ExampleService;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,6 +27,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -51,5 +56,28 @@ public class ExampleControllerUnitTests  extends AbstractControllerTest {
         doNothing().when(exampleService).setExampleValidator(any(MultipartFile.class),anyLong());
         doNothing().when(exampleService).deleteExample(anyLong());
         doNothing().when(exampleService).deleteExampleValidator(anyLong());
+    }
+
+    @Test
+    public void check_all_apis_unauthorized_no_jwt_token() throws Exception {
+
+        //get /fileTypes   		getFileTypes
+        this.mvc.perform(get("/api/fileTypes").accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized());
+        //get /example/{id}		getExample
+        this.mvc.perform(get("/api/example/200").accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized());
+        //get /example/{id}/validator	getExampleValidator
+        this.mvc.perform(get("/api/example/200/validator").accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized());
+        //put /example			createExample
+        this.mvc.perform(put("/api/example").accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized());
+        //post /example			updateExample
+        this.mvc.perform(post("/api/example").accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized());
+        //post /example/order		updateExampleOrder
+        this.mvc.perform(post("/api/example/order").accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized());
+        //post /example/validator	setExampleValidator
+        this.mvc.perform(post("/api/example/validator").accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized());
+        //delete /example/{id}		deleteExample
+        this.mvc.perform(delete("/api/example/200").accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized());
+        //delete /example/{id}/validator delete ExampleValidator
+        this.mvc.perform(delete("/api/example/200/validator").accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized());
     }
 }
