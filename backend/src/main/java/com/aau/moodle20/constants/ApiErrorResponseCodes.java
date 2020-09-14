0409917ed8334fbe51fa734653510b03c69bb0e7
yodiack <pipp.peter@gmx.at>
2020-09-14 18:18:32 +0200

    make interface a class again , and add private constructor

--- backend/src/main/java/com/aau/moodle20/constants/ApiErrorResponseCodes.java
+++ backend/src/main/java/com/aau/moodle20/constants/ApiErrorResponseCodes.java
@@ -1,26 +1,29 @@
 package com.aau.moodle20.constants;
 
-public interface ApiErrorResponseCodes {
+public class ApiErrorResponseCodes {
 
-    Integer INVALID_METHOD_PARAMETER = 460;
-    Integer API_NOT_FOUND = 461;
-    Integer USER_ALREADY_EXISTS = 462;
+    private ApiErrorResponseCodes() {
+    }
+
+    public static final Integer INVALID_METHOD_PARAMETER = 460;
+    public static final Integer API_NOT_FOUND = 461;
+    public static final Integer USER_ALREADY_EXISTS = 462;
 
 
-    Integer USERNAME_ALREADY_EXISTS = 470;
-    Integer MATRICULATION_NUMBER_ALREADY_EXISTS = 471;
-    Integer SEMESTER_ALREADY_EXISTS = 472;
-    Integer COURSE_IN_SEMESTER_ALREADY_EXISTS = 473;
-    Integer CHANGED_COURSE_NUMBER_ALREADY_EXISTS = 474;
-    Integer COPIED_COURSE_NUMBER_ALREADY_EXISTS = 475;
-    Integer TEMPORARY_PASSWORD_EXPIRED = 480;
+    public static final Integer USERNAME_ALREADY_EXISTS = 470;
+    public static final Integer MATRICULATION_NUMBER_ALREADY_EXISTS = 471;
+    public static final Integer SEMESTER_ALREADY_EXISTS = 472;
+    public static final Integer COURSE_IN_SEMESTER_ALREADY_EXISTS = 473;
+    public static final Integer CHANGED_COURSE_NUMBER_ALREADY_EXISTS =474;
+    public static final Integer COPIED_COURSE_NUMBER_ALREADY_EXISTS =475;
+    public static final Integer TEMPORARY_PASSWORD_EXPIRED = 480;
 
-    Integer REGISTER_USERS_WRONG_MATRICULATION_NUMBER_FORMAT = 481;
-    Integer REGISTER_USERS_MATRICULATION_ALREADY_EXISTS = 482;
-    Integer REGISTER_USERS_USERNAME_ALREADY_EXISTS = 483;
-    Integer EXERCISE_SHEET_WITH_THIS_NAME_ALREADY_EXISTS = 484;
+    public static final Integer REGISTER_USERS_WRONG_MATRICULATION_NUMBER_FORMAT = 481;
+    public static final Integer REGISTER_USERS_MATRICULATION_ALREADY_EXISTS = 482;
+    public static final Integer REGISTER_USERS_USERNAME_ALREADY_EXISTS = 483;
+    public static final Integer EXERCISE_SHEET_WITH_THIS_NAME_ALREADY_EXISTS = 484;
 
-    Integer EXAMPLE_WITH_THIS_NAME_ALREADY_EXISTS = 485;
-    Integer SUB_EXAMPLE_WITH_THIS_NAME_ALREADY_EXISTS = 486;
-    Integer NO_VALID_VALIDATOR_FOUND_IN_JAR = 487;
+    public static final Integer EXAMPLE_WITH_THIS_NAME_ALREADY_EXISTS = 485;
+    public static final Integer SUB_EXAMPLE_WITH_THIS_NAME_ALREADY_EXISTS = 486;
+    public static final Integer NO_VALID_VALIDATOR_FOUND_IN_JAR = 487;
 }
