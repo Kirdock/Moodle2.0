94f2c338848961e1f2001f1b651a0e391e65b5c8
yodiack <pipp.peter@gmx.at>
2020-09-25 14:07:34 +0200

    remove unused code

--- backend/src/main/java/com/aau/moodle20/security/jwt/JwtUtils.java
+++ backend/src/main/java/com/aau/moodle20/security/jwt/JwtUtils.java
@@ -3,8 +3,6 @@ package com.aau.moodle20.security.jwt;
 import com.aau.moodle20.services.UserDetailsImpl;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -13,14 +11,10 @@ import java.util.Date;
 
 @Component
 public class JwtUtils {
-
-    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
-
     public static final String CLAIM_IS_ADMIN = "isAdmin";
     public static final String CLAIM_MATRICULATION_NUMBER = "matriculationNumber";
     public static final String CLAIM_FORENAME = "forename";
     public static final String CLAIM_SURNAME = "surname";
-
 
     @Value("${jwtSecret}")
     private String jwtSecret;
@@ -48,30 +42,8 @@ public class JwtUtils {
         return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
     }
 
-    public String getMatriculationNumberFromJwtToken(String token) {
-        return (String) Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().get(CLAIM_MATRICULATION_NUMBER);
-    }
-
-    public Boolean getAdminFromJwtToken(String jwtToken) {
-        return (Boolean) Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwtToken).getBody().get(CLAIM_IS_ADMIN);
-    }
-
-    public boolean validateJwtToken(String authToken) throws Exception {
-//        try {
+    public boolean validateJwtToken(String authToken){
         Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
         return true;
-//        } catch (SignatureException e) {
-//            logger.error("Invalid JWT signature: {}", e.getMessage());
-//        } catch (MalformedJwtException e) {
-//            logger.error("Invalid JWT token: {}", e.getMessage());
-//        } catch (ExpiredJwtException e) {
-//            logger.error("JWT token is expired: {}", e.getMessage());
-//        } catch (UnsupportedJwtException e) {
-//            logger.error("JWT token is unsupported: {}", e.getMessage());
-//        } catch (IllegalArgumentException e) {
-//            logger.error("JWT claims string is empty: {}", e.getMessage());
-//        }
-
-//        return false;
     }
 }
